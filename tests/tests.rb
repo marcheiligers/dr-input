ba962cebfc5cb442f5509b702f1d3bab53a86de3
Marc Heiligers <marc@eternal.co.za>
2023-09-02 16:52:33 -0700

    Fix tests

--- tests/tests.rb
+++ tests/tests.rb
@@ -29,65 +29,65 @@ def test_calcstringbox_tab_has_no_witdh(_args, assert)
 end
 
 def test_find_word_breaks_empty_value(_args, assert)
-  assert.equal! Input.new.find_word_breaks(''), ['']
+  assert.equal! Input::Multiline.new.find_word_breaks(''), ['']
 end
 
 def test_find_word_breaks_single_space(_args, assert)
-  assert.equal! Input.new.find_word_breaks(' '), [' ']
+  assert.equal! Input::Multiline.new.find_word_breaks(' '), [' ']
 end
 
 def test_find_word_breaks_single_char(_args, assert)
-  assert.equal! Input.new.find_word_breaks('a'), ['a']
+  assert.equal! Input::Multiline.new.find_word_breaks('a'), ['a']
 end
 
 def test_find_word_breaks_2_words(_args, assert)
-  assert.equal! Input.new.find_word_breaks('hello, world'), ['hello, ', 'world']
+  assert.equal! Input::Multiline.new.find_word_breaks('hello, world'), ['hello, ', 'world']
 end
 
 def test_find_word_breaks_leading_and_trailing_white_space(_args, assert)
-  assert.equal! Input.new.find_word_breaks(" \t  hello \t "), [" \t  hello \t "]
+  assert.equal! Input::Multiline.new.find_word_breaks(" \t  hello \t "), [" \t  hello \t "]
 end
 
 def test_find_word_breaks_leading_and_trailing_white_space_multiple_words(_args, assert)
-  assert.equal! Input.new.find_word_breaks(" \t  hello, \t  world \t"), [" \t  hello, \t  ", "world \t"]
+  assert.equal! Input::Multiline.new.find_word_breaks(" \t  hello, \t  world \t"), [" \t  hello, \t  ", "world \t"]
 end
 
 def test_find_word_breaks_new_line(_args, assert)
-  assert.equal! Input.new.find_word_breaks("hello, \n  world"), ['hello, ', "\n  world"]
+  assert.equal! Input::Multiline.new.find_word_breaks("hello, \n  world"), ['hello, ', "\n  world"]
 end
 
 def test_find_word_breaks_double_new_line(_args, assert)
-  assert.equal! Input.new.find_word_breaks("hello, \n\n  world"), ['hello, ', "\n", "\n  world"]
+  assert.equal! Input::Multiline.new.find_word_breaks("hello, \n\n  world"), ['hello, ', "\n", "\n  world"]
 end
 
 def test_find_word_breaks_multiple_new_lines(_args, assert)
-  assert.equal! Input.new.find_word_breaks("1\n\n\n2"), ['1', "\n", "\n", "\n2"]
+  assert.equal! Input::Multiline.new.find_word_breaks("1\n\n\n2"), ['1', "\n", "\n", "\n2"]
 end
 
 def test_find_word_breaks_trailing_new_line(_args, assert)
-  assert.equal! Input.new.find_word_breaks("1\n"), ['1', "\n"]
+  assert.equal! Input::Multiline.new.find_word_breaks("1\n"), ['1', "\n"]
 end
 
 def test_perform_word_wrap_multiple_new_lines(_args, assert)
-  assert.equal! Input.new.perform_word_wrap(['1', "\n", "\n", "\n2"]).map(&:text), ['1', "\n", "\n", "\n2"]
+  assert.equal! Input::Multiline.new.perform_word_wrap(['1', "\n", "\n", "\n2"]).map(&:text), ['1', "\n", "\n", "\n2"]
 end
 
 def test_perform_word_wrap_trailing_new_line(_args, assert)
-  assert.equal! Input.new.perform_word_wrap(['1', "\n"]).map(&:text), ['1', "\n"]
+  assert.equal! Input::Multiline.new.perform_word_wrap(['1', "\n"]).map(&:text), ['1', "\n"]
 end
 
 def test_find_word_breaks_trailing_new_line_after_wrap(_args, assert)
-  assert.equal! Input.new.find_word_breaks("1234567890 1234567890 1234567890\n"), ['1234567890 ', '1234567890 ', '1234567890', "\n"]
+  assert.equal! Input::Multiline.new.find_word_breaks("1234567890 1234567890 1234567890\n"), ['1234567890 ', '1234567890 ', '1234567890', "\n"]
 end
 
 def test_perform_word_wrap_trailing_new_line_after_wrap(_args, assert)
-  assert.equal! Input.new.perform_word_wrap(['1234567890 ', '1234567890 ', '1234567890', "\n"]).map(&:text), ['1234567890 1234567890 ', '1234567890', "\n"]
+  assert.equal! Input::Multiline.new.perform_word_wrap(['1234567890 ', '1234567890 ', '1234567890', "\n"]).map(&:text), ['1234567890 1234567890 ', '1234567890', "\n"]
 end
 
 def test_find_word_breaks_trailing_new_line_after_wrap_with_space(_args, assert)
-  assert.equal! Input.new.find_word_breaks("1234567890 1234567890 \n"), ['1234567890 ', '1234567890 ', "\n"]
+  assert.equal! Input::Multiline.new.find_word_breaks("1234567890 1234567890 \n"), ['1234567890 ', '1234567890 ', "\n"]
 end
 
 # def test_perform_word_wrap_trailing_new_line_after_wrap(args, assert)
-#   assert.equal! Input.new.perform_word_wrap(['', '']), ["1\n", '']
+#   assert.equal! Input::Multiline.new.perform_word_wrap(['', '']), ["1\n", '']
 # end
