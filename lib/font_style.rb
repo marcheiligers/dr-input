50bce45cdca72ed126a8e42d47bb1f46824ddd3b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-05-17 11:02:28 +0900

    Introduce FontStyle::UsingSizeEnum class

--- lib/font_style.rb
+++ lib/font_style.rb
@@ -1,25 +1,27 @@
-class FontStyle
+module FontStyle
   def self.from(**params)
-    new(**params)
+    UsingSizeEnum.new(**params)
   end
 
-  attr_reader :font_height
+  class UsingSizeEnum
+    attr_reader :font_height
 
-  def initialize(font:,  word_chars:, size_enum: nil, size_px: nil)
-    @font = font
-    @label_params = size_px ? { size_px: size_px } : { size_enum: size_enum }
-    _, @font_height = $gtk.calcstringbox(word_chars.join(''), font: @font, **@label_params)
-  end
+    def initialize(font:,  word_chars:, size_enum: nil, size_px: nil)
+      @font = font
+      @label_params = size_px ? { size_px: size_px } : { size_enum: size_enum }
+      _, @font_height = $gtk.calcstringbox(word_chars.join(''), font: @font, **@label_params)
+    end
 
-  def string_width(str)
-    $gtk.calcstringbox(str, font: @font, **@label_params)[0]
-  end
+    def string_width(str)
+      $gtk.calcstringbox(str, font: @font, **@label_params)[0]
+    end
 
-  def label(values)
-    {
-      font: @font,
-      **@label_params,
-      **values
-    }
+    def label(values)
+      {
+        font: @font,
+        **@label_params,
+        **values
+      }
+    end
   end
 end
