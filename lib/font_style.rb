6f07173c80edb0aa5020080fa9a401bb10b4fa90
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-05-17 10:47:15 +0900

    Rewrite instance variable inside FontStyle

--- lib/font_style.rb
+++ lib/font_style.rb
@@ -3,15 +3,19 @@ class FontStyle
 
   def initialize(font:, size_enum:, word_chars:)
     @font = font
-    @size_enum = size_enum
-    _, @font_height = $gtk.calcstringbox(word_chars.join(''), @size_enum, @font)
+    @label_params = { size_enum: size_enum }
+    _, @font_height = $gtk.calcstringbox(word_chars.join(''), font: @font, **@label_params)
   end
 
   def string_width(str)
-    $gtk.calcstringbox(str, @size_enum, @font)[0]
+    $gtk.calcstringbox(str, font: @font, **@label_params)[0]
   end
 
   def label(values)
-    { size_enum: @size_enum, font: @font, **values }
+    {
+      font: @font,
+      **@label_params,
+      **values
+    }
   end
 end
