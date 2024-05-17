e21dd6bc4f981247902f1418fcb1336bc1caf362
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-05-17 11:05:58 +0900

    Separate size_px and size_enum classes

--- lib/font_style.rb
+++ lib/font_style.rb
@@ -1,27 +1,43 @@
 module FontStyle
   def self.from(**params)
+    return UsingSizePx.new(**params) if params[:size_px]
+
     UsingSizeEnum.new(**params)
   end
 
   class UsingSizeEnum
     attr_reader :font_height
 
-    def initialize(font:,  word_chars:, size_enum: nil, size_px: nil)
+    def initialize(font:,  word_chars:, size_enum:)
       @font = font
-      @label_params = size_px ? { size_px: size_px } : { size_enum: size_enum }
-      _, @font_height = $gtk.calcstringbox(word_chars.join(''), font: @font, **@label_params)
+      @size_enum = size_enum
+      _, @font_height = $gtk.calcstringbox(word_chars.join(''), @size_enum, @font)
     end
 
     def string_width(str)
-      $gtk.calcstringbox(str, font: @font, **@label_params)[0]
+      $gtk.calcstringbox(str, @size_enum, @font)[0]
     end
 
     def label(values)
-      {
-        font: @font,
-        **@label_params,
-        **values
-      }
+      { font: @font, size_enum: @size_enum, **values }
+    end
+  end
+
+  class UsingSizePx
+    attr_reader :font_height
+
+    def initialize(font:,  word_chars:, size_px:)
+      @font = font
+      @size_px = size_px
+      _, @font_height = $gtk.calcstringbox(word_chars.join(''), font: @font, size_px: @size_px)
+    end
+
+    def string_width(str)
+      $gtk.calcstringbox(str, font: @font, size_px: @size_px)[0]
+    end
+
+    def label(values)
+      { font: @font, size_px: @size_px, **values }
     end
   end
 end
